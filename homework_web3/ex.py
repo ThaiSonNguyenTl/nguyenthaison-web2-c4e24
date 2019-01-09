46f7022afd4506bb75e15d5ad087632fdc4c7c1d
CHÀO ANH SƠN Ạ <thaisonnguyentl@gmail.com>
2019-01-10 00:38:24 +0700

    initial commit

---
+++ homework_web3/ex.py
@@ -0,0 +1,14 @@
+import mlab
+from models.river import River
+
+mlab.connect()
+
+print("ex2")
+river_list = River.objects(continent__iexact = "Africa")
+for river in river_list:
+    print(river.continent)
+
+print("ex3")
+river_list = River.objects(continent__iexact = "S. America" , length__lt = 1000)
+for river in river_list:
+    print(river.continent,river.length)
